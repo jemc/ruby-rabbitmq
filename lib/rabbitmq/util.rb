722c50dcd0cd675950304b6bffa9ee722b0bcdc3
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-05-30 09:18:28 -0700

    Add block form to Util.error_check.

--- lib/rabbitmq/util.rb
+++ lib/rabbitmq/util.rb
@@ -4,7 +4,8 @@ class RabbitMQ::FFI::Error < RuntimeError; end
 module RabbitMQ::Util
   class << self
     
-    def error_check rc
+    def error_check rc=nil
+      rc ||= yield
       return if rc == 0
       raise RabbitMQ::FFI::Error, RabbitMQ::FFI.amqp_error_string2(rc)
     end
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -13,5 +13,13 @@ describe RabbitMQ::Util do
     it "raises an error when given a nonzero result code" do
       expect { subject.error_check(-1) }.to raise_error RabbitMQ::FFI::Error
     end
+    
+    it "returns nil when given a zero result code in a block" do
+      subject.error_check { 0 }.should eq nil
+    end
+    
+    it "raises an error when given a nonzero result code in a block" do
+      expect { subject.error_check { -1 } }.to raise_error RabbitMQ::FFI::Error
+    end
   end
 end
