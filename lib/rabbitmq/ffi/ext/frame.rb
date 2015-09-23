d27425efd074871bfe3d5eb553343f0ce2f032df
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-09-23 08:08:11 -0700

    Raise FFI::Error::BadAmqpData in case of unexpected frames.

--- lib/rabbitmq/ffi/ext/frame.rb
+++ lib/rabbitmq/ffi/ext/frame.rb
@@ -14,26 +14,26 @@ module RabbitMQ
       end
       
       def as_method_to_h(free=false)
-        # TODO: raise correct error class with enough info for appropriate action
-        raise "Wrong frame type for method frame of event: #{self[:frame_type]}" \
-          unless self[:frame_type] == :method
+        raise FFI::Error::BadAmqpData,
+          "Wrong frame type for method frame of event: #{self[:frame_type]}" \
+            unless self[:frame_type] == :method
         
         payload.to_h(free).merge(channel: self[:channel])
       end
       
       def as_header_to_h(free=false)
-        # TODO: raise correct error class with enough info for appropriate action
-        raise "Wrong frame type for header frame of multiframe event: #{self[:frame_type]}" \
-          unless self[:frame_type] == :header
+        raise FFI::Error::BadAmqpData,
+          "Wrong frame type for header frame of multiframe event: #{self[:frame_type]}" \
+            unless self[:frame_type] == :header
         
         properties = self[:payload][:properties]
         { header: properties.decoded.to_h(free), body_size: properties[:body_size] }
       end
       
       def as_body_to_s(free=false)
-        # TODO: raise correct error class with enough info for appropriate action
-        raise "Wrong frame type for body frame of multiframe event: #{self[:frame_type]}" \
-          unless self[:frame_type] == :body
+        raise FFI::Error::BadAmqpData,
+          "Wrong frame type for body frame of multiframe event: #{self[:frame_type]}" \
+            unless self[:frame_type] == :body
         
         self[:payload][:body_fragment].to_s(free)
       end
